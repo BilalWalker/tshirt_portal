f5c13d36e59c06476efd0cb7e3ffaf09634d1f3c
Walker <bilalwalker687@gmail.com>
2025-05-15 20:32:50 +0500

    add migrations and create basic model file for users

--- tshirt_portal/urls.py
+++ tshirt_portal/urls.py
@@ -23,9 +23,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Role(models.TextChoices):
+    ADMIN = 'ADMIN', _('Admin')
+    DESIGNER = 'DESIGNER', _('Designer')
+    VIEWER = 'VIEWER', _('Viewer')
+
+class CustomUser(AbstractUser):
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.DESIGNER,
+    )
+    bio = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+    def is_admin(self):
+        return self.role == Role.ADMIN
+    
+    def is_designer(self):
+        return self.role == Role.DESIGNER or self.role == Role.ADMIN
